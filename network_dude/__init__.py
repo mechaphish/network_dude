483ebb902c2d9532600ad3ac1013b035f0a44d23
Aravind, Machiry <machiry@cs.ucsb.edu>
2016-06-03 22:15:44 -0700

    Adding all

--- network_dude/__init__.py
+++ network_dude/__init__.py
@@ -1,5 +1,6 @@
 """
-
+This module handles parsing data provided by DARPA UDP packets
+into pickled objects and writes them to DB do after each round.
 """
 import os
 import struct
@@ -7,73 +8,38 @@ import socket
 import time
 import thread
 import pickle
+import threading
 from dotenv import load_dotenv
+from common_utils.simple_logging import *
 load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
-
-import simple_logging
-
-
-def log_error(msg):
-    """
-    Log error message
-    :param msg: Message to be logged
-    :return: None
-    """
-    print("[!] " + str(msg))
-
-
-def log_info(msg):
-    """
-    Log info message
-    :param msg: Message to be logged
-    :return: None
-    """
-    print("[*] " + str(msg))
-
-
-def log_success(msg):
-    """
-    Log success message
-    :param msg: Message to be logged
-    :return: None
-    """
-    print("[+] " + str(msg))
-
-
-def log_failure(msg):
-    """
-    Log failure message
-    :param msg: Message to be logged
-    :return: None
-    """
-    print("[-] " + str(msg))
-
-# END TODO
 
 
 class Connection(object):
+    """
+    Class that handles incomming packets.
+    """
 
     CLIENT, SERVER = (0, 1)
     HEADER_LEN = 15
     LOG_EVERY_PKT_KEY = 'LOG_EVERY_PACKET'
 
-    def __init__(self, port, datafolder):
+    def __init__(self, port, data_folder):
         """
-
-        :param port:
-        :param datafolder:
-        :return:
+            Create a connection object.
+        :param port: Port number to listen
+        :param data_folder: folder in which the incoming data needs to be saved.
         """
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
         self.sock.bind(('', port))
 
-        self.datafolder = datafolder
+        self.data_folder = data_folder
         self.curr_out_filename = None
         self.curr_out_file = None
         self.last_pkt_received_at = None
         self.log_every_packet = False
+        self.curr_file_lock = threading.Lock()
         if Connection.LOG_EVERY_PKT_KEY in os.environ:
             if int(os.environ[Connection.LOG_EVERY_PKT_KEY]):
                 self.log_every_packet = True
@@ -81,9 +47,12 @@ class Connection(object):
         log_info("logging network traffic from port:" + str(port) + " to folder:" + str(self.datafolder))
 
     def start_listening(self):
+        log_info("Starting to listen.")
         while True:
+            # Receive data
             data = self.sock.recvfrom(0xFFFF)[0]
             self.last_pkt_received_at = time.time()
+            # parse the contents.
             packet = self.parse(data)
             if packet is None:
                 continue
@@ -93,45 +62,42 @@ class Connection(object):
             if self.log_every_packet:
                 log_info("csid: " + str(csid) + " connection: " + str(connection_id) + " message_id: " + str(msg_id) +
                          " side: " + str(side))
-
+            # Write the parsed data to file
             self.write_packet(packet)
-
-    def open_pcap(self, filename):
-        if filename is None:
-            return None
-
-        PCAP_MAGIC = 0xa1b2c3d4L
-        LINK_TYPE = 1  # ethernet
-
-        PCAP_VERSION_MAJOR = 2
-        PCAP_VERSION_MINOR = 4
-        if filename is None:
-            return None
-
-        pcap_handle = open(filename, 'w')
-        header = struct.pack('>IHHIIII', PCAP_MAGIC, PCAP_VERSION_MAJOR,
-                             PCAP_VERSION_MINOR, 0, 0, 1500, LINK_TYPE)
-
-        pcap_handle.write(header)
-        return pcap_handle
 
     def write_packet(self, packet):
         """
-
-        :param packet:
-        :return:
+            Write provided data as pickled object into file
+        :param packet: packet to be written to file
+        :return: None
         """
-        if self.curr_out_file is None or self.curr_out_filename is None:
-            self.curr_out_filename = os.path.join(self.datafolder, str(time.time()) + '_network_log')
-            self.curr_out_file = open(self.curr_out_filename, 'wb')
-        pickle.dump(packet, self.curr_out_file)
-        self.curr_out_file.flush()
+        try:
+            # Try to obtain lock.
+            # but the lock should be non-blocking.
+            # This way we will be fast and doesn't slow down the receiving thread.
+            if self.curr_file_lock.acquire(blocking=False):
+                if self.curr_out_file is None or self.curr_out_filename is None:
+                    self.curr_out_filename = os.path.join(self.datafolder, str(time.time()) + '_network_log')
+                    self.curr_out_file = open(self.curr_out_filename, 'wb')
+                pickle.dump(packet, self.curr_out_file)
+                self.curr_out_file.flush()
+                self.curr_file_lock.release()
+            else:
+                log_error("Unable to obtain lock on current file, ignoring the packet.")
+        except Exception as e:
+            try:
+                # Try to release the lock, to avoid deadlocks.
+                self.curr_file_lock.release()
+            except Exception as e1:
+                # Ignore all exceptions.
+                pass
+            log_error("Unexpected error occurred while trying to write packet to file:" + str(e))
 
     def parse(self, data):
         """
-
-        :param data:
-        :return:
+            Parse the provided message into various fields.
+        :param data: Data to be parsed.
+        :return: csid, connection_id, msg_id, side, message
         """
         if len(data) < Connection.HEADER_LEN:
             log_error("invalid message length: " + str(len(data)))
@@ -151,12 +117,6 @@ class Connection(object):
 
         return csid, connection_id, msg_id, side, message
 
-    def log(self, data):
-        if self.pcap_file is None:
-            return
-
-        pass
-
 DEFAULT_DATA_FOLDER = "queue"
 MIN_IDLE_TIME = 15
 DEFAULT_IDLE_TIME = 20
@@ -168,8 +128,11 @@ DATA_FOLDER_KEY = 'QUEUE_FOLDER'
 
 def do_setup():
     """
-
-    :return:
+        Perform initial setup.
+        1. Get IDLE time.
+        2. Get Data folder. (and clean it).
+        3. Get port number.
+    :return: round_idle_time, data_folder, listen_port
     """
     # Setup idle time between rounds
     round_idle_time = DEFAULT_IDLE_TIME
@@ -199,9 +162,9 @@ def do_setup():
 
 def data_dumper_thread(connection_object, idle_time_threshold):
     """
-
-    :param connection_object:
-    :param idle_time_threshold:
+        Thread which writes the collected data to DB.
+    :param connection_object: Connection object, which needs to be monitored.
+    :param idle_time_threshold: Idle threshold time.
     :return:
     """
     log_info("Starting Data Dumper Thread.")
@@ -211,21 +174,41 @@ def data_dumper_thread(connection_object, idle_time_threshold):
     while True:
         curr_time = time.time()
         idle_time = curr_time - connection_object.last_pkt_received_at
-        if idle_time >= idle_time_threshold and connection_object.curr_out_file is not None:
-            target_file_name = connection_object.curr_out_filename
-            connection_object.curr_out_file.close()
-            connection_object.curr_out_file = None
-            connection_object.curr_out_filename = None
-            log_info("Dumping the file:" + str(target_file_name) + " into DB.")
-            # TODO: Dump the data to DB
+        if idle_time >= idle_time_threshold:
+            # This means potentially current round has ended.
+            try:
+                target_file_name = None
+                # blocking acquire
+                connection_object.curr_file_lock.acquire()
+                if connection_object.curr_out_file is not None:
+                    # close the current files
+                    target_file_name = connection_object.curr_out_filename
+                    connection_object.curr_out_file.close()
+                    connection_object.curr_out_file = None
+                    connection_object.curr_out_filename = None
+                # release file locks.
+                connection_object.curr_file_lock.release()
+                if target_file_name is not None:
+                    log_info("Dumping the file:" + str(target_file_name) + " into DB.")
+                    # TODO: Dump the data to DB
+            except Exception as e:
+                try:
+                    # To avoid deadlocks.
+                    connection_object.curr_file_lock.release()
+                except Exception as e1:
+                    pass
+                log_error("Error occurred while trying to save the dump file to DB:" + str(e))
         else:
             time.sleep(poll_time)
 
 
 def main():
     round_idle_time, data_folder, listen_port = do_setup()
+    # Create connection object.
     conn_obj = Connection(listen_port, data_folder)
+    # Start the data dumper thread.
     thread.start_new_thread(data_dumper_thread, (conn_obj, round_idle_time, ))
+    # Continue listening.
     conn_obj.start_listening()
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='network_dude',
     version='0.0.1',
-    packages=[ 'network_dude' ],
+    packages=['network_dude'],
     scripts=['bin/network_dude'],
     install_requires=[i.strip() for i in open('requirements.txt').readlines() if 'git' not in i],
     description='Continuous traffic processor from DARPA.',
